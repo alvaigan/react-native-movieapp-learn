6901982b724991c6be7418a9c822385a7ee78154
Alvaigan <aalvaigan@gmail.com>
2025-05-04 19:32:23 +0700

    feat(navigation): load navigation bar assets

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,14 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// create a component
+const Saved= () => {
+    return (
+        <View>
+            <Text>Saved</Text>
+        </View>
+    );
+};
+
+export default Saved;
---
+++ app/movies/[id].tsx
@@ -0,0 +1,14 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// create a component
+const MovieDetails= () => {
+    return (
+        <View>
+            <Text>MovieDetails</Text>
+        </View>
+    );
+};
+
+export default MovieDetails;
